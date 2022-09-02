855fe85c338608fdf0211d1816cc7105b312e0f1
Sebastián <slanzetti@sanagustinsuperior.edu.ar>
2022-09-02 19:30:18 -0300

    cambio de ruta de acceso al back

--- config/constantes.php
+++ config/constantes.php
@@ -1,7 +1,7 @@
 <?php
 
 	define("CONFIG_ROOT_SITE",'http://'.getenv("HTTP_HOST").'/TisaFrontPhp'); // root del sitio
-	define("API_URL",'http://127.0.0.1/Tisa2022BackEnd');// host de la api
+	define("API_URL",'http://127.0.0.1/Tisa/Tisa2022BackEnd');// host de la api
 	define("HEADER_FILE",'header.php');// host de la api
 	define("FOOTER_FILE",'footer.php');// host de la api
 
